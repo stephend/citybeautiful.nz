47ca5b01924a1d5472d9fdcd49152d113a0a45dd
Stephen Davis <stephend@gmail.com>
2017-04-02 00:28:32 +1300

    windows support for generate_images, do not regenerate existing, new 960px size, rewrite

--- images/generate_images.sh
+++ images/generate_images.sh
@@ -7,6 +7,44 @@ else
 	export MAGICK_CONVERT="magick convert"
 fi
 
+crush_finished() {
+	if [ ${I_EXT} == 'png' ]; then
+		pngcrush -ow -q ${O_NAME}
+	else
+		jpegoptim --strip-all -q -m 90 ${O_NAME}
+	fi
+}
+
+convert_tn() {
+	export O_NAME="${I_DIR}/${I_FIRST}.tn${1}.${I_EXT}"
+	if [ ! -e ${O_NAME} -a $I_WIDTH -ge ${1} ] ; then
+		$MAGICK_CONVERT $I_FULLNAME \
+			-strip -resize "${1}x${1}^" -gravity center -crop 144x144+0+0 +repage \
+			"${O_NAME}"
+		crush_finished
+	fi	
+}
+
+convert_w_jpg() {
+	export O_NAME="${I_DIR}/${I_FIRST}.w${1}.jpg"
+	if [ ! -e ${O_NAME} -a $I_WIDTH -ge ${1} ] ; then
+		$MAGICK_CONVERT $I_FULLNAME \
+			-resize "${1}x" \
+			"${O_NAME}"
+		crush_finished
+	fi	
+}
+
+convert_w() {
+	export O_NAME="${I_DIR}/${I_FIRST}.w${1}.${I_EXT}"
+	if [ ! -e ${O_NAME} -a $I_WIDTH -ge ${1} ] ; then
+		$MAGICK_CONVERT $I_FULLNAME \
+			-resize "${1}x" \
+			"${O_NAME}"
+		crush_finished
+	fi
+}
+
 find -regextype posix-egrep -regex '.*\.orig\.(jpg|jpeg|gif|png)$' | while read line; do
 	export I_FULLNAME=$line
 	export I_BASENAME=`basename $I_FULLNAME`
@@ -14,35 +52,17 @@ find -regextype posix-egrep -regex '.*\.orig\.(jpg|jpeg|gif|png)$' | while read 
 	export I_EXT=${I_BASENAME##*.orig.}
 	export I_FIRST=${I_BASENAME%.orig.*}
 
-	echo "${I_DIR}/${I_FIRST} (${I_EXT})" 
 
-	$MAGICK_CONVERT $I_FULLNAME \
-		-strip -resize "144x144^" -gravity center -crop 144x144+0+0 +repage \
-		"${I_DIR}/${I_FIRST}.tn144.${I_EXT}"
-	$MAGICK_CONVERT $I_FULLNAME \
-		-strip -resize "300x300^" -gravity center -crop 300x300+0+0 +repage \
-		"${I_DIR}/${I_FIRST}.tn300.${I_EXT}"
-	$MAGICK_CONVERT $I_FULLNAME \
-		-resize "600x" \
-		"${I_DIR}/${I_FIRST}.w600.jpg"
-	$MAGICK_CONVERT $I_FULLNAME \
-		-resize "800x" \
-		"${I_DIR}/${I_FIRST}.w800.${I_EXT}"
-	$MAGICK_CONVERT $I_FULLNAME \
-		-resize "800x" \
-		"${I_DIR}/${I_FIRST}.w960.${I_EXT}"
-done
+	# Clear existing files
+	rm ${I_DIR}/${I_FIRST}.{tn144,tn300,w600,w800,w960}.*
 
-find -regextype posix-egrep -regex '.*\.(tn144|tn300|w600|w800|w960)\.(png)$' | while read line; do
-	#echo $line
-	#ls -l $line
-	pngcrush -ow -q $line 
-	#ls -l $line
-done
+	read -r I_WIDTH I_HEIGHT <<< $( $MAGICK_CONVERT $I_FULLNAME -format "%w %h" info:)
 
-find -regextype posix-egrep -regex '.*\.(tn144|tn300|w600|w800|w960)\.(jpg|jpeg)$' | while read line; do
-	#echo $line
-	#ls -l $line
-	jpegoptim --strip-all -q -m 90 $line 
-	#ls -l $line
+	echo "${I_DIR}/${I_FIRST} (${I_EXT}, ${I_WIDTH}x${I_HEIGHT})" 
+
+	convert_tn 144
+	convert_tn 300
+	convert_w_jpg 600
+	convert_w 800
+	convert_w 960
 done
